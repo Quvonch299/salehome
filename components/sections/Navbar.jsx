0dcf0062c662d37f78d67d7a95d1215bde0b783a
Quvonch299 <muminovquvonchbek85@gmail.com>
2025-12-09 20:14:47 +0500

    email

--- components/sections/Navbar.jsx
+++ components/sections/Navbar.jsx
@@ -13,17 +13,29 @@ export default function Navbar() {
                 </span>
 
             </div>
-            <div  className='text-[#654D31] font-bold flex gap-14 max-sm:gap-[16px]'>
-                <span className='flex gap-[6px] items-center'>
-                    <FaPhone size={24}/>
-                    <h2 className='font-semibold text-[18px] leading-[100%] tracking-[0%] max-sm:hidden'>+7 978 221 26 88</h2>
-                </span>
-                <span className='flex gap-[6px] items-center'>
-                <MdEmail size={24} />
+           <div className='text-[#654D31] font-bold flex gap-14 max-sm:gap-[16px]'>
 
-                    <h2 className='font-semibold text-[18px] leading-[100%] tracking-[0%] max-sm:hidden'>info@gidrealter.ru</h2>
-                </span>
-            </div>
+  <a 
+    href="tel:+79782212688"
+    className="flex gap-[6px] items-center hover:underline"
+  >
+    <FaPhone size={24} />
+    <h2 className='font-semibold text-[18px] leading-[100%] tracking-[0%] max-sm:hidden'>
+      +7 978 221 26 88
+    </h2>
+  </a>
+<a target='blank'
+  href="momiovquvonch2@gmail.com"
+  className="flex gap-[6px] items-center hover:underline"
+>
+  <MdEmail size={24} />
+  <h2 className='font-semibold text-[18px] leading-[100%] tracking-[0%] max-sm:hidden'>
+    momiovquvonch2@gmail.com
+  </h2>
+</a>
+
+
+</div>
             <div>
                 <button className='w-[224px] h-[64px] max-sm:w-[192px] max-sm:h-[56px] max-sm:text-[16px] text-[#654D31] border rounded-[15px] bg-[#FFFF] border-[2px]'>Заказать звонок</button>
             </div>
